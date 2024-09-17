567774d381d7ff6690e11142f9b893b6cd254435
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-17 18:39:59 +0800

    👽️ added api for random photos

--- src/api/pictureApi.tsx
+++ src/api/pictureApi.tsx
@@ -17,3 +17,25 @@ export async function getRandomPicture() {
         console.log(error)
     }
 }
+
+
+export async function getPhotos(){
+    const url = `https://api.unsplash.com/photos/random?count=10`;
+    const options = {
+        method: 'GET',
+        headers: {
+            Authorization: `Client-ID ${import.meta.env.VITE_ACCESS_KEY}`,
+        }
+    }
+
+    try {
+        const response = await fetch(url, options)
+        if(!response.ok){
+            throw new Error(`Error: ${response.status}`);
+        }
+        const result = await response.json();
+        return result
+    } catch (error) {
+        console.log(error)
+    }
+}
