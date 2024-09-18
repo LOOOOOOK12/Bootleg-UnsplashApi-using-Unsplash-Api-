ad549773eaaf0acdfc2ddc40e3a24fdbcdb167f7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-18 17:31:12 +0800

    ✅ added handle search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import NavBar from './components/nav';
 import * as PictureApi from './api/pictureApi';
 import './App.css';
 
 function App() {
   // const [pictureData, setPictureData] = useState<any>(null);
   const [pictureListData, setPictureListData] = useState<any[]>([]);
+  const [searchValue, setSearchValue] = useState<string>("")
+  const searchInput = useRef(null)
 
   const listPictures = async () => {
     try {
@@ -15,18 +18,25 @@ function App() {
     }
   }
 
+  const handleSearch = (event:React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    setSearchValue(event.target.value)
+  }
+
   useEffect(() => {
     listPictures();
   }, []);
 
   return (
-    <div className='flex flex-wrap gap-3'>
-      {pictureListData.map((pic) =>(
-        <div key={pic.id} className='flex flex-col gap-2'>
-          <img key={pic.id} src={pic.urls.raw} alt={pic.description} className='h-52' />
-          <h1>{pic.description}</h1>
-        </div>
-      ))}
+    <div className=''>
+      <NavBar handleSearch={handleSearch}/>
+      <div className='max-h-screen flex flex-wrap gap-3'>
+        {pictureListData.map((pic) =>(
+          <div key={pic.id} className='flex flex-col gap-2'>
+            <img key={pic.id} src={pic.urls.raw} alt={pic.description} className='h-52' />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
