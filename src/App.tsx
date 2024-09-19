05bfe80672a834c811b4beb828ae1014b2ab53ae
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-19 21:12:45 +0800

    💄 add: anchor tag for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from './components/nav';
+import picturePage from './pages/picturePage';
 import * as PictureApi from './api/pictureApi';
 import './App.css';
 
 function App() {
-  // const [pictureData, setPictureData] = useState<any>(null);
   const [pictureListData, setPictureListData] = useState<any[]>([]);
   const [searchValue, setSearchValue] = useState<string>("")
-  const searchInput = useRef(null)
 
   const listPictures = async () => {
     try {
       const result = await PictureApi.getPhotos();    
       setPictureListData(result);
+      console.log(result)
     } catch (error) { 
       console.log(error)
     }
@@ -23,17 +23,19 @@ function App() {
     setSearchValue(event.target.value)
   }
 
-  useEffect(() => {
-    listPictures();
-  }, []);
+  // useEffect(() => {
+  //   listPictures();
+  // }, []);
 
   return (
-    <div className=''>
+    <div>
       <NavBar handleSearch={handleSearch}/>
       <div className='max-h-screen flex flex-wrap gap-3'>
         {pictureListData.map((pic) =>(
           <div key={pic.id} className='flex flex-col gap-2'>
-            <img key={pic.id} src={pic.urls.raw} alt={pic.description} className='h-52' />
+            <a href={pic.links.html} target='_blank'>
+              <img key={pic.id} src={pic.urls.raw} alt={pic.description} className='h-52' />
+            </a>
           </div>
         ))}
       </div>
