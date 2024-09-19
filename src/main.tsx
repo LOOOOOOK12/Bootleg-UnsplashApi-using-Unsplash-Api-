cbe635f3f6b7dfc1b782e5102a70cfe1c3e6674d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-19 21:14:35 +0800

    ✅  add: Routes are applied

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <RouterProvider router={router} />
 )
