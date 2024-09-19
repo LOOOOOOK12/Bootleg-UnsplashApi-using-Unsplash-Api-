3852c32ef1da5b2d9fd66c7c6f01bb229a575cb3
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-19 21:16:26 +0800

    💄 add: container for image link

---
+++ src/pages/picturePage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import NavBar from '../components/nav'
+
+type PicturePageProps = {
+    image: string;
+    imageDescription: string;
+    location: string;    
+}
+
+function picturePage() {
+    return (
+        <div>
+            <NavBar/>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default picturePage
