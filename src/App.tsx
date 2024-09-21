2fef0e3ccc31a9c1ec61f341a9192d3fb13b787a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-21 16:29:04 +0800

    ✅ added Link for passing data to the PicturePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,48 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import NavBar from './components/nav';
-import picturePage from './pages/picturePage';
 import * as PictureApi from './api/pictureApi';
 import './App.css';
 
 function App() {
   const [pictureListData, setPictureListData] = useState<any[]>([]);
-  const [searchValue, setSearchValue] = useState<string>("")
+  const [searchValue, setSearchValue] = useState<string>("");
 
   const listPictures = async () => {
     try {
-      const result = await PictureApi.getPhotos();    
+      const result = await PictureApi.getPhotos();
       setPictureListData(result);
-      console.log(result)
-    } catch (error) { 
-      console.log(error)
+      console.log(result);
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
-  const handleSearch = (event:React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    setSearchValue(event.target.value)
-  }
+    setSearchValue(event.target.value);
+  };
 
-  // useEffect(() => {
-  //   listPictures();
-  // }, []);
+  useEffect(() => {
+    listPictures();
+  }, []);
 
   return (
     <div>
-      <NavBar handleSearch={handleSearch}/>
-      <div className='max-h-screen flex flex-wrap gap-3'>
-        {pictureListData.map((pic) =>(
-          <div key={pic.id} className='flex flex-col gap-2'>
-            <a href={pic.links.html} target='_blank'>
-              <img key={pic.id} src={pic.urls.raw} alt={pic.description} className='h-52' />
-            </a>
-          </div>
+      <NavBar handleSearch={handleSearch} />
+      <div className="max-h-screen flex flex-wrap gap-3">
+        {pictureListData.map((pic) => (
+          <Link
+            key={pic.id}
+            to={`/photo/${pic.id}`}
+            state={{ 
+              image: pic.urls.raw, 
+              imageDescription: pic.description, 
+              place: pic.location.name, 
+            }}
+          >
+            <img src={pic.urls.raw} alt={pic.description} className="h-52" />
+          </Link>
         ))}
       </div>
     </div>
