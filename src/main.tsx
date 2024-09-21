abb07b5393c5eefb80c46dbebd1844e5d435400e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-21 16:30:14 +0800

    ✅ added PicturePage routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import PicturePage from './pages/picturePage.tsx'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element: <App/>
+    element: <App/>,
   },
+  {
+    path:"/photo/:pictureId",
+    element:<PicturePage/>,
+  }
 ]);
 
 createRoot(document.getElementById('root')!).render(
