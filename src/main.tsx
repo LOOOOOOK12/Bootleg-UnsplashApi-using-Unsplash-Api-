879a224317139e68eaf5f6f2e401d45bf40045bc
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-22 17:10:59 +0800

    🔥 removed: react ipmort

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react'
 import App from './App.tsx'
 import PicturePage from './pages/picturePage.tsx'
 import './index.css'
