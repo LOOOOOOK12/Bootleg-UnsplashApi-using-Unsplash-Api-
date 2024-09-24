6fcffe1d06a22b78eb62a3d7ac78ec2e07375222
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-24 17:53:59 +0800

    💄 update:  dialog is changed to popover

--- src/components/searchCard.tsx
+++ src/components/searchCard.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
-import { Card, CardContent } from "@/components/ui/card";
-
-type Topic = {
-    id: string;
-    slug: string;
-    title: string;
-};
+import { Input } from './ui/input';
+import { Topics } from '../types/types';
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
 
 type SearchCardProps = {
-    topics?: Topic[]; 
-}
+    handleSearch?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    topicsData?: Topics[];
+};
 
-function SearchCard({ topics }: SearchCardProps) {
+function SearchCard({ handleSearch, topicsData }: SearchCardProps) {
     return (
-        <Card className='absolute left-8 top-12 w-[30rem] h-[10rem]'>
-            <CardContent>
-                {topics && topics.length > 0 ? (
-                    topics.map((topic) => (
-                        <p key={topic.id} className='text-black'>{topic.title}</p>
-                    ))
-                ) : (
-                    <p className='text-black'>No topics available</p>
-                )}
-            </CardContent>
-        </Card>
+        <Popover>
+            <div className="w-full">
+                <PopoverTrigger asChild>
+                    <Input
+                        placeholder="Search Photos and illustrations"
+                        type="search"
+                        onChange={handleSearch}
+                        className="w-full border border-black"
+                    />
+                </PopoverTrigger>
+            </div>
+            <PopoverContent sideOffset={5} align="start">
+                <div className="flex flex-col">
+                    <h1>Topics</h1>
+                    {topicsData && topicsData.length > 0 ? (
+                        topicsData.map((topic, idx) => (
+                            <div key={idx} className="flex">
+                                <p className="text-black">{topic.title}</p>
+                            </div>
+                        ))
+                    ) : (
+                        <p className="text-black">No topics available</p>
+                    )}
+                </div>
+            </PopoverContent>
+        </Popover>
     );
 }
 
