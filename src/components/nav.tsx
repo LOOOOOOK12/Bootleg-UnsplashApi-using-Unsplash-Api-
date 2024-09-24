785a775bd52b0764cf67581cddfc6fb3e124465e
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-24 17:53:31 +0800

    🎨 update: Input is changed to popover import 💄 update: nav color changed and justify is removed 🔥 removed: type is removed, and modalstate is removed

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,40 +1,22 @@
 import React, { useState } from 'react';
-import { Input } from './ui/input';
 import SearchCard from './searchCard';
-
-type Topic = {
-    id: string;
-    slug: string;
-    title: string;
-};
+import { Topics } from '../types/types' 
 
 type NavProps = {
     handleSearch?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    topicsData?: Topic[]; // Change to Topic[]
+    topicData?: Topics[]; 
 };
 
-function Nav({ handleSearch, topicsData }: NavProps) {
-    const [openSearchModal, setOpenSearchModal] = useState<boolean>(false);
-
-    const handleSearchModalOnClick = () => {
-        setOpenSearchModal(true);
-    };
-
+function Nav({ handleSearch, topicData }: NavProps) {
     return (
-        <div className='sticky z-10 top-0 flex items-center justify-between bg-slate-400 py-2 px-5 gap-5 w-full border-b-[#2e2e2e]'>
+        <div className='sticky z-10 top-0 flex items-center bg-lightMode-background py-2 px-5 gap-5 w-full'>
             <h1>LOAK</h1>
-            <div className='flex w-full gap-2'>
-                <Input
-                    placeholder="Search Photos and illustrations"
-                    type="search"
-                    onChange={handleSearch}
-                    onClick={handleSearchModalOnClick}
-                    className='w-1/2 border border-black'
+            <div className='w-1/2'>
+                <SearchCard
+                    handleSearch={handleSearch}
+                    topicsData={topicData}
                 />
             </div>
-            {openSearchModal && (
-                <SearchCard topics={topicsData} />
-            )}
         </div>
     );
 }
