6aaa05df4af5ff5fca016c791b9e5f76a225b54d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-24 23:00:14 +0800

    🏷️ updates: types for topic data 💄 update: styling changes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SearchCard from './searchCard';
 import { Topics } from '../types/types' 
-
+import { Image } from 'lucide-react';
 type NavProps = {
     handleSearch?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    topicData?: Topics[]; 
+    topicData: Topics[]; 
 };
 
 function Nav({ handleSearch, topicData }: NavProps) {
     return (
-        <div className='sticky z-10 top-0 flex items-center bg-lightMode-background py-2 px-5 gap-5 w-full'>
-            <h1>LOAK</h1>
+        <div className='sticky z-10 top-0 flex items-center border border-b-gray-400 bg-lightMode-background py-2 px-5 gap-5 w-full'>
+            <Image/>
             <div className='w-1/2'>
                 <SearchCard
                     handleSearch={handleSearch}
