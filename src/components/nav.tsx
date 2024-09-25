308a443b91a1ac7a3a60913219342a979162d97f
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-25 21:30:53 +0800

    💄 update: added darkmode button

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import SearchCard from './searchCard';
 import { Topics } from '../types/types' 
-import { Image } from 'lucide-react';
+import { Image, Moon, Sun } from 'lucide-react';
+
 type NavProps = {
+    handleSearchData?: string[];
     handleSearch?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    topicData: Topics[]; 
+    topicData?: Topics[];
+    handleDarkMode?: ()=> void;
+    darkMode?: boolean;
 };
 
-function Nav({ handleSearch, topicData }: NavProps) {
+function Nav({ handleSearch, topicData, handleSearchData, handleDarkMode, darkMode }: NavProps) {
     return (
-        <div className='sticky z-10 top-0 flex items-center border border-b-gray-400 bg-lightMode-background py-2 px-5 gap-5 w-full'>
-            <Image/>
-            <div className='w-1/2'>
-                <SearchCard
-                    handleSearch={handleSearch}
-                    topicsData={topicData}
-                />
+        <nav className='sticky z-10 top-0 bg-lightMode-background flex items-center justify-between border border-x-0 border-t-0 border-b-gray-400 dark:bg-darkMode-colors-background dark:border-b-lightMode-background py-2 px-5 gap-5 w-full duration-200'>
+            <div className='flex flex-row w-full items-center gap-2'>
+                <a href="#Home">{darkMode? <Image color='#f9fafb'/>: <Image color='#040506'/>}</a>
+                <div className='w-1/2'>
+                    <SearchCard
+                        handleSearchData={handleSearchData}
+                        handleSearch={handleSearch}
+                        topicsData={topicData}
+                        darkMode={darkMode}
+                    />
+                </div>
             </div>
-        </div>
+            <button onClick={handleDarkMode}>
+                {darkMode ? <Sun color="#f9fafb" /> : <Moon color='#040506'/>}
+            </button>
+        </nav>
     );
 }
 
