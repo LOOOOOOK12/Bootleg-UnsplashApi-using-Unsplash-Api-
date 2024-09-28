4dd576dcf48ef23932b15bee2b96212fa36c1280
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-09-28 21:21:11 +0800

    🔥 removed: searchcard is removed

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import SearchCard from './searchCard';
-import { Topics } from '../types/types' 
+import React, { useRef } from 'react';
+import { Input } from './ui/input';
 import { Image, Moon, Sun } from 'lucide-react';
 
 type NavProps = {
-    handleSearchData?: string[];
-    handleSearch?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    topicData?: Topics[];
+    handleSearch?: (value: string) => void;
     handleDarkMode?: ()=> void;
     darkMode?: boolean;
 };
 
-function Nav({ handleSearch, topicData, handleSearchData, handleDarkMode, darkMode }: NavProps) {
+function Nav({ handleSearch, handleDarkMode, darkMode }: NavProps) {
+    const searchInputRef = useRef<HTMLInputElement>(null);
+
+    const onSearchSubmit = () => {
+        if (handleSearch && searchInputRef.current) {
+          handleSearch(searchInputRef.current.value); // Pass the input value to the handler
+        }
+    };
+
     return (
         <nav className='sticky z-10 top-0 bg-lightMode-background flex items-center justify-between border border-x-0 border-t-0 border-b-gray-400 dark:bg-darkMode-colors-background dark:border-b-lightMode-background py-2 px-5 gap-5 w-full duration-200'>
             <div className='flex flex-row w-full items-center gap-2'>
                 <a href="#Home">{darkMode? <Image color='#f9fafb'/>: <Image color='#040506'/>}</a>
                 <div className='w-1/2'>
-                    <SearchCard
-                        handleSearchData={handleSearchData}
-                        handleSearch={handleSearch}
-                        topicsData={topicData}
-                        darkMode={darkMode}
-                    />
+                    <form onSubmit={onSearchSubmit}> {/* Wrap in a form and handle onSubmit */}
+                        <Input
+                        ref={searchInputRef} // Attach the ref to the input
+                        placeholder="Search photos and illustrations"
+                        type="search"
+                        className={`w-full border ${
+                            darkMode
+                            ? 'dark:bg-darkMode-colors-background dark:text-darkMode-colors-text dark:border-darkMode-colors-text'
+                            : 'border-gray-400'
+                        } duration-200`}
+                        />
+                    </form>
                 </div>
             </div>
             <button onClick={handleDarkMode}>
