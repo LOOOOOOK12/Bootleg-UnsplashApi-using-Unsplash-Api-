6e57cba505cc8c21d0e9acfdaf59b7c88fbb3c91
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-02 19:45:52 +0800

    ⚡️ dark mode is now useable to any compnent

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,36 @@
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import PicturePage from './pages/picturePage.tsx';
+import SearchPage from './pages/searchPage.tsx';
+import { useState } from 'react';
+import './index.css';
 
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import React from 'react'
-import App from './App.tsx'
-import PicturePage from './pages/picturePage.tsx'
-import './index.css'
+function Main() {
+  const [darkMode, setDarkMode] = useState(false);
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <App/>,
-  },
-  {
-    path:"/photo/:pictureId",
-    element:<PicturePage/>,
-  }
-]);
+  const toggleDarkmode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <App darkMode={darkMode} toggleDarkmode={toggleDarkmode} />,
+    },
+    {
+      path: '/photo/:pictureId',
+      element: <PicturePage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />,
+    },
+    {
+      path: '/search/:query',
+      element: <SearchPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
+  <Main />
+);
