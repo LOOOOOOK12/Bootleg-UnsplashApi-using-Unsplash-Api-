42e06d8e1fe1834065b6dd354b0ef0bf91bdc097
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-02 19:46:55 +0800

    ⚡️ route to the home is now made

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,43 +1,43 @@
 import React, { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
 import { Input } from './ui/input';
 import { Image, Moon, Sun } from 'lucide-react';
-
-type NavProps = {
-    handleSearch?: (value: string) => void;
-    handleDarkMode?: ()=> void;
-    darkMode?: boolean;
-};
+import { NavProps } from '@/types/types';
 
 function Nav({ handleSearch, handleDarkMode, darkMode }: NavProps) {
     const searchInputRef = useRef<HTMLInputElement>(null);
+    const navigate = useNavigate(); 
 
-    const onSearchSubmit = () => {
+    const onSearchSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
         if (handleSearch && searchInputRef.current) {
-            handleSearch(searchInputRef.current.value);
+            const searchQuery = searchInputRef.current.value;
+            handleSearch(searchQuery);
+            navigate(`/search/${searchQuery}`);     
         }
     };
 
     return (
         <nav className='sticky z-10 top-0 bg-lightMode-background flex items-center justify-between border border-x-0 border-t-0 border-b-gray-400 dark:bg-darkMode-colors-background dark:border-b-lightMode-background py-2 px-5 gap-5 w-full duration-200'>
             <div className='flex flex-row w-full items-center gap-2'>
-                <a href="#Home">{darkMode? <Image color='#f9fafb'/>: <Image color='#040506' className='h-full'/>}</a>
+                <Link to={`/`}>
+                    {darkMode ? <Image color='#f9fafb' /> : <Image color='#040506' className='h-full' />}
+                </Link>
                 <div className='w-1/2'>
-                    <form onSubmit={onSearchSubmit}> 
+                    <form onSubmit={onSearchSubmit}>
                         <Input
-                        ref={searchInputRef}
-                        placeholder="Search photos and illustrations"
-                        type="search"
-                        className={`w-full border ${
-                            darkMode
-                            ? 'dark:bg-darkMode-colors-background dark:text-darkMode-colors-text dark:border-darkMode-colors-text'
-                            : 'border-gray-400'
-                        } duration-200`}
+                            ref={searchInputRef}
+                            placeholder="Search photos and illustrations"
+                            type="search"
+                            className={`w-full border ${darkMode
+                                ? 'dark:bg-darkMode-colors-background dark:text-darkMode-colors-text dark:border-darkMode-colors-text'
+                                : 'border-gray-400'} duration-200`}
                         />
                     </form>
                 </div>
             </div>
             <button onClick={handleDarkMode}>
-                {darkMode ? <Sun color="#f9fafb" /> : <Moon color='#040506'/>}
+                {darkMode ? <Sun color="#f9fafb" /> : <Moon color='#040506' />}
             </button>
         </nav>
     );
