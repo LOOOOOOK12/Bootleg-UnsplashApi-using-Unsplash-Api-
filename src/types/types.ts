9d5cd9f34ccb9f75368167de59ba14f07af929a8
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-06 16:32:26 +0800

    🎨 removed unused types and navprops and darkmode props are in one prop

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,27 +1,10 @@
-export type Topics = {
-    id: string;
-    slug: string;
-    title: string;
-    cover_photo : {
-        urls: {
-            thumb: string;
-        }
-    }
-};
-
-export type Search = {
-    results: {
-        
-    }
-}
-
 export type NavProps = {
     handleSearch?: (value: string) => void;
-    handleDarkMode?: () => void;
-    darkMode?: boolean;
 };
 
 export type DarkModeProps = {
-    darkMode: boolean; 
-    toggleDarkmode: () => void;
+    darkMode?: boolean; 
+    toggleDarkmode?: () => void;
 }
+
+export type NavBarProps = NavProps & DarkModeProps
