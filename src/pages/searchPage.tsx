2e227b4aa0aa58b929bea34d4ed277d4d7bb0a77
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-06 16:31:44 +0800

    ✅ added useNavigate to navigate to another query after search

--- src/pages/searchPage.tsx
+++ src/pages/searchPage.tsx
@@ -1,62 +1,71 @@
 import { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import * as PictureApi from '../api/pictureApi';
 import { Skeleton } from '../components/ui/skeleton';
 import NavBar from '../components/nav';
-import { DarkModeProps } from '@/types/types';
+import { NavBarProps } from '@/types/types';
 
-function SearchPage({ darkMode, toggleDarkmode }: DarkModeProps) {
+function SearchPage({ darkMode, toggleDarkmode, handleSearch }: NavBarProps) {
     const { query } = useParams<{ query: string }>();
     const [searchData, setSearchData] = useState<any[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchSearchResults = async () => {
-        try {
-            setIsLoading(true);
-            const result = await PictureApi.searchPictures(query!);
-            setSearchData(result);
-            console.log(result);
-        } catch (error) {
-            console.log("Error fetching search results:", error);
-        } finally {
-            setIsLoading(false);
-        }
+            if (query) {
+                try {
+                    setIsLoading(true);
+                    const result = await PictureApi.searchPictures(query);
+                    setSearchData(result);
+                } catch (error) {
+                    console.log("Error fetching search results:", error);
+                } finally {
+                    setIsLoading(false);
+                }
+            }
         };
 
-        if (query) {
         fetchSearchResults();
-        }
     }, [query]);
 
     return (
         <div className={`flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
-            <NavBar handleDarkMode={toggleDarkmode} darkMode={darkMode} />
-            <div className={`flex flex-wrap justify-center items-center gap-3 py-8 bg-lightMode-background dark:bg-darkMode-colors-background`}>
+            <NavBar 
+                toggleDarkmode={toggleDarkmode} 
+                darkMode={darkMode} 
+                handleSearch={(newQuery) => {
+                    if (handleSearch) {
+                        handleSearch(newQuery);
+                        navigate(`/search/${newQuery}`);
+                    }
+                }} 
+            />
+            <div className="max-h-full flex flex-wrap overflow-hidden justify-center gap-5 py-8 px-5 dark:bg-darkMode-colors-background duration-200">
                 {isLoading ? (
-                Array(30)
-                    .fill(0)
-                    .map((_, idx) => <Skeleton key={idx} className="h-52 w-60" />)
+                    Array(30)
+                        .fill(0)
+                        .map((_, idx) => <Skeleton key={idx} className="h-52 w-60" />)
                 ) : (
-                searchData.map((searchPic) => (
-                    <Link
-                    key={searchPic.id}
-                    to={`/photo/${searchPic.id}`}
-                    state={{
-                        image: searchPic.urls.raw,
-                        imageDescription: searchPic.description,
-                        place: searchPic.location ? searchPic.location.name : 'Unknown',
-                    }}
-                    >
-                    <img
-                        id={searchPic.id}
-                        src={searchPic.urls.raw}
-                        alt={searchPic.description || 'Image'}
-                        title={searchPic.description || 'No description'}
-                        className="h-52"
-                    />
-                    </Link>
-                ))
+                    searchData.map((searchPic) => (
+                        <Link
+                            key={searchPic.id}
+                            to={`/photo/${searchPic.id}`}
+                            state={{
+                                image: searchPic.urls.raw,
+                                imageDescription: searchPic.description,
+                                place: searchPic.location ? searchPic.location.name : 'Unknown',
+                            }}
+                        >
+                            <img
+                                id={searchPic.id}
+                                src={searchPic.urls.full}
+                                alt={searchPic.description || 'Image'}
+                                title={searchPic.description || 'No description'}
+                                className="w-full h-80 grow"
+                            />
+                        </Link>
+                    ))
                 )}
             </div>
         </div>
