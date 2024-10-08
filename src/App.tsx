704e94bae953a0acf917838adf1a7d22029dcaeb
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-08 18:24:57 +0800

    ✅ added page state, added next and prev page functions, added next and prev buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Skeleton } from './components/ui/skeleton';
+import { ChevronLeft, ChevronRight  } from 'lucide-react';
 import NavBar from './components/nav';
 import { DarkModeProps } from './types/types';
 import * as PictureApi from './api/pictureApi';
@@ -10,6 +11,7 @@ function App({ darkMode, toggleDarkmode }: DarkModeProps ) {
   const [pictureListData, setPictureListData] = useState<any[]>([]);
   const [searchData, setSearchData] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [page, setPage] = useState(1);
 
   const listPictures = async () => {
     try {
@@ -23,6 +25,30 @@ function App({ darkMode, toggleDarkmode }: DarkModeProps ) {
       setIsLoading(false);
     }
   };
+
+  const handleNextPage = async () => {
+    try {
+      setPage((prevCount) => {
+        const newPage = prevCount + 1;
+        console.log("Current page:", newPage); 
+        return newPage;
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handlePrevPage = async () => {
+    try {
+      setPage((prevCount) => {
+        const newPage = prevCount > 0 ? prevCount - 1 : 0;
+        console.log("Current page:", newPage); 
+        return newPage;
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const handleSearch = async (value: string) => {
     if (value.trim() === "") {
@@ -46,7 +72,7 @@ function App({ darkMode, toggleDarkmode }: DarkModeProps ) {
   return (
     <div className={`flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
       <NavBar handleSearch={handleSearch} toggleDarkmode={toggleDarkmode} darkMode={darkMode} />
-      <div id="Home" className="max-h-full flex flex-wrap overflow-hidden justify-center gap-3 py-8 px-5 dark:bg-darkMode-colors-background duration-200" >
+      <div id="Home" className="max-h-full relative flex flex-wrap overflow-hidden justify-center gap-3 py-8 px-5 dark:bg-darkMode-colors-background duration-200" >
         {isLoading ? (
           Array(30)
             .fill(0)
@@ -74,6 +100,10 @@ function App({ darkMode, toggleDarkmode }: DarkModeProps ) {
             </Link>
           ))
         )}
+      <div className='fixed bottom-5 right-5 flex gap-2 z-20 '>
+        <button onClick={handlePrevPage} className={`border-2 border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Previous Page'>{ darkMode ?<ChevronLeft color='#f9fafb' size={50}/> : <ChevronLeft color='#040506' size={50}/>}</button>
+        <button onClick={handleNextPage} className={`border-2 border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Next Page'>{ darkMode ?<ChevronRight color='#f9fafb' size={50}/> : <ChevronRight color='#040506' size={50}/>}</button>
+      </div>
       </div>
     </div>
   );
