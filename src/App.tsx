224c7f07c08215681660aee4ec567a5410d5d1b7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-08 19:00:18 +0800

    💄 added PageButtons components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Skeleton } from './components/ui/skeleton';
-import { ChevronLeft, ChevronRight  } from 'lucide-react';
 import NavBar from './components/nav';
 import { DarkModeProps } from './types/types';
 import * as PictureApi from './api/pictureApi';
+import PageButtons from './components/pageButtons';
 import './App.css';
 
 function App({ darkMode, toggleDarkmode }: DarkModeProps ) {
@@ -70,9 +70,9 @@ function App({ darkMode, toggleDarkmode }: DarkModeProps ) {
   }, []);
 
   return (
-    <div className={`flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
+    <div className={`relative flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
       <NavBar handleSearch={handleSearch} toggleDarkmode={toggleDarkmode} darkMode={darkMode} />
-      <div id="Home" className="max-h-full relative flex flex-wrap overflow-hidden justify-center gap-3 py-8 px-5 dark:bg-darkMode-colors-background duration-200" >
+      <div id="Home" className="max-h-full flex flex-wrap overflow-hidden justify-center gap-3 py-8 px-5 dark:bg-darkMode-colors-background duration-200" >
         {isLoading ? (
           Array(30)
             .fill(0)
@@ -100,13 +100,10 @@ function App({ darkMode, toggleDarkmode }: DarkModeProps ) {
             </Link>
           ))
         )}
-      <div className='fixed bottom-5 right-5 flex gap-2 z-20 '>
-        <button onClick={handlePrevPage} className={`border-2 border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Previous Page'>{ darkMode ?<ChevronLeft color='#f9fafb' size={50}/> : <ChevronLeft color='#040506' size={50}/>}</button>
-        <button onClick={handleNextPage} className={`border-2 border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Next Page'>{ darkMode ?<ChevronRight color='#f9fafb' size={50}/> : <ChevronRight color='#040506' size={50}/>}</button>
       </div>
-      </div>
+      <PageButtons toggleNextPage={handleNextPage} togglePrevPage={handlePrevPage} darkMode={darkMode}/>
     </div>
   );
-}
+};
 
 export default App;
