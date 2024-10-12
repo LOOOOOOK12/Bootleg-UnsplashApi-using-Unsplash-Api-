70fc5b953ab1fe1d7d22078430ae97d9efdb7f10
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-12 22:25:29 +0800

    💄 added likes

--- src/pages/picturePage.tsx
+++ src/pages/picturePage.tsx
@@ -8,11 +8,12 @@ function PicturePage({ darkMode, toggleDarkmode, handleSearch }: NavBarProps) {
     const navigate = useNavigate(); 
     const state = location.state as PicturePageProps | undefined;
     console.log(state);
+
     if (!state) {
         return <div>Error: No picture data available</div>; 
     }
 
-    const { image, imageDescription, place} = state;
+    const { image, imageDescription, place, likes} = state;
 
     const handleSearchAndNavigate = (newQuery: string) => {
         if (handleSearch) {
@@ -22,17 +23,18 @@ function PicturePage({ darkMode, toggleDarkmode, handleSearch }: NavBarProps) {
     };
 
     return (
-        <div className={` bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
+        <div className={`bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
             <NavBar
                 toggleDarkmode={toggleDarkmode}
                 darkMode={darkMode}
                 handleSearch={handleSearchAndNavigate}
             />
-            <div  className={`bg-lightMode-background dark:bg-darkMode-colors-background h-screen flex flex-col items-center justify-center px-5`}>
+            <div  className={`h-screen bg-lightMode-background dark:bg-darkMode-colors-background flex flex-col items-center justify-center px-5`}>
                 <img src={image} alt={imageDescription} className='h-80' />
                 <div className='flex flex-col'>
                     <p className='text-lightMode-text dark:text-darkMode-colors-text'>{imageDescription}</p>
                     <p className='text-lightMode-text dark:text-darkMode-colors-text'>{place}</p>
+                    <p>Likes {likes}</p>
                 </div>
             </div>
         </div>
