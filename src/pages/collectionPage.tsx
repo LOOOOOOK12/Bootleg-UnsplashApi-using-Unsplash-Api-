fc8baeef94584706297fc54471bdd1ffbd4f989d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-21 17:21:03 +0800

    ⚡️ applied the handleSearch function

--- src/pages/collectionPage.tsx
+++ src/pages/collectionPage.tsx
@@ -1,13 +1,21 @@
 import { useLocation, useNavigate } from 'react-router-dom'
 import { NavBarProps, CollectionPageProps } from '@/types/types'
 import NavBar from '../components/nav'
+import { CircleUserRound, UserRound } from 'lucide-react';
 
-function collectionPage({darkMode, toggleDarkmode}: NavBarProps) {
+function collectionPage({ darkMode, toggleDarkmode, handleSearch }: NavBarProps) {
     const location = useLocation();
     const navigate = useNavigate();
     const state = location.state as CollectionPageProps | undefined;
 
     console.log(state)
+
+    const handleSearchAndNavigate = (newQuery: string) => {
+        if (handleSearch) {
+            handleSearch(newQuery);
+            navigate(`/search/${newQuery}`);
+        }
+    };
     
     if(!state){
         return <div>Error: No collection data available</div>; 
@@ -18,15 +26,16 @@ function collectionPage({darkMode, toggleDarkmode}: NavBarProps) {
     return (
         <div className={`bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
             <NavBar
-                darkMode = {darkMode}
                 toggleDarkmode={toggleDarkmode}
+                darkMode={darkMode}
+                handleSearch={handleSearchAndNavigate}
             />
-            <div className='flex flex-col justify-center h-screen items-center gap-2 dark:bg-darkMode-colors-background'>
+            <div className='flex flex-col justify-center h-screen items-center gap-2 bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text'>
                 <img src={image} alt={title} className='h-80 w-60' />
                 <p>Title: {title}</p>
                 <p>Description: {description}</p>
                 <p>Total photos: {totalPhotos}</p>
-                <p>Username: {user}</p>
+                <p className='flex gap-2'><CircleUserRound/> {user}</p>
             </div>
         </div>
     )
