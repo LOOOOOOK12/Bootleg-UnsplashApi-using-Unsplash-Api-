9e8d947656471fd4233b7847004482b45082cc85
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-26 16:47:04 +0800

    🦺 added validation if slug exists

--- src/hooks/useGetTopicPhotos.ts
+++ src/hooks/useGetTopicPhotos.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import * as PictureApi from "../api/pictureApi"
 
-export default function useGetTopicPhotos(page: Number = 1, slug?:string){
+export default function useGetTopicPhotos(page: Number, slug?:string){
     const [ topicPhotos, setTopicPhotos ] = useState<any[]>([]);
     const [ isLoading, setIsLoading ] = useState<boolean>(false);
 
     const fetchTopicPhotos = async() => {
+        if (!slug) return;
         try {
             setIsLoading(true);
             const result = await PictureApi.getTopicPhotos(page, slug);
