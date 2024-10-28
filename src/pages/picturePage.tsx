58687d06d0dcbd4cdb71a5ae829ffc4ff348d4b7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-28 19:10:09 +0800

    ⚡️ added color and testing validation depending on routes for data

--- src/pages/picturePage.tsx
+++ src/pages/picturePage.tsx
@@ -1,6 +1,6 @@
 import { NavBarProps, PicturePageProps } from '@/types/types';
 import NavBar from '../components/nav';
-
+import { ThumbsUpIcon } from 'lucide-react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 function PicturePage({ darkMode, toggleDarkmode, handleSearch }: NavBarProps) {
@@ -13,7 +13,7 @@ function PicturePage({ darkMode, toggleDarkmode, handleSearch }: NavBarProps) {
         return <div>Error: No picture data available</div>; 
     }
 
-    const { image, imageDescription, place, likes} = state;
+    const { image, imageDescription, place, likes, color} = state;
 
     const handleSearchAndNavigate = (newQuery: string) => {
         if (handleSearch) {
@@ -29,12 +29,13 @@ function PicturePage({ darkMode, toggleDarkmode, handleSearch }: NavBarProps) {
                 darkMode={darkMode}
                 handleSearch={handleSearchAndNavigate}
             />
-            <div  className={` max-h-* bg-lightMode-background dark:bg-darkMode-colors-background flex flex-col items-center justify-center px-5 py-8`}>
-                <img src={image} alt={imageDescription} className='h-80' />
-                <div className='flex flex-col'>
-                    <p className='text-lightMode-text dark:text-darkMode-colors-text'>{imageDescription}</p>
-                    <p className='text-lightMode-text dark:text-darkMode-colors-text'>{place}</p>
-                    <p>Likes {likes}</p>
+            <div  className={`h-screen bg-lightMode-background dark:bg-darkMode-colors-background flex flex-col items-center justify-center px-5 py-8`}>
+                <img src={image} alt={imageDescription} className='h-80 object-cover' />
+                <div className='flex flex-col text-lightMode-text dark:text-darkMode-colors-text'>
+                    <p>{imageDescription}</p>
+                    <p>{place}</p>
+                    <p><ThumbsUpIcon/>{likes}</p>
+                    {color ? (<p>color: {color}</p>):""}
                 </div>
             </div>
         </div>
