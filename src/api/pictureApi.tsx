01f1fe9dbb79673190cd4f791fa89fe653e2daa4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-10-31 18:26:52 +0800

    🔥 removed random pictures api

--- src/api/pictureApi.tsx
+++ src/api/pictureApi.tsx
@@ -1,26 +1,5 @@
 const Auth = `Client-ID ${import.meta.env.VITE_ACCESS_KEY}`;
 
-//GetRandomPicture
-export async function getRandomPicture() {
-    const url = `https://api.unsplash.com/photos/random`;
-    const options = {
-        method: 'GET',
-        headers: {
-            Authorization: Auth,
-        }
-    }
-    try {
-        const response = await fetch(url, options)
-        if (!response.ok) {
-            throw new Error(`Error: ${response.status}`);
-        }
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 export async function getPhotos(page?: Number) {
     const url = `https://api.unsplash.com/photos?per_page=30&page=${page}`;
     const options = {
@@ -43,31 +22,7 @@ export async function getPhotos(page?: Number) {
     }
 }
 
-export async function getRandomPhotos(orientation?: string,){
-    const url = `https://api.unsplash.com/photos/random?count=10&orientation=landscape`;
-    const options = {
-        method: 'GET',
-        headers: {
-            "cache-control": "public, max-age=1000",
-            Authorization: Auth,
-        }
-    }
-
-    try {
-        const response = await fetch(url, options)
-        if(!response.ok){
-            throw new Error(`Error: ${response.status}`);
-        }
-        const result = await response.json();
-        return result
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 export async function getTopics() {
-    // const random = Math.floor(Math.random() * 4) + 1;
-    // console.log(random);
     const url = `https://api.unsplash.com/topics?page=1&per_page=5&order_by=featured`;
     const options = {
         method: 'GET',
@@ -136,6 +91,7 @@ export async function getCollections(page: Number = 1){
     try {
         const response = await fetch(url, options);
         const result = await response.json();
+        console.log(result);
         return result;
     } catch (error) {
         console.log(error);
