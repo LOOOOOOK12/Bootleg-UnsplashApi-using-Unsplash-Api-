2d8c776acd347b094343eccb4735d65993922d42
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-02 22:38:39 +0800

    ✅ added useGetCollection custom hook

---
+++ src/hooks/useGetCollectionPhoto.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import * as PictureApi from '../api/pictureApi';
+
+export default function useGetCollectionPhoto(page: Number = 1 , id?: any){
+    const [ collectionPhotos, setCollectionPhotos ] = useState<any[]>([]);
+    const [ isloading, setIsLoading ] = useState<boolean>(false);
+
+    const fetchCollectionPhotos = async () => {
+        if(!id) return;
+        
+        try {
+            setIsLoading(true);
+            const result = await PictureApi.getCollectionPhotos(page, id);
+            setCollectionPhotos(result);
+            console.log(result);
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        fetchCollectionPhotos();
+    },[page, id]);
+
+    return { isloading, collectionPhotos};
+}
