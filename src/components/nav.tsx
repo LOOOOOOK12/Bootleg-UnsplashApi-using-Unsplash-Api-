bc95a39924442b5ce78a801420dde87811036cbf
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-05 21:26:45 +0800

    💄 border color changes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -33,7 +33,7 @@ function Nav({ handleSearch, toggleDarkmode, darkMode }: NavBarProps) {
     }, [searchQuery, location]);
 
     return (
-        <nav className='sticky z-10 top-0 bg-lightMode-background flex flex-col justify-between border border-x-0 border-t-0 border-b-gray-400 dark:bg-darkMode-colors-background dark:border-b-lightMode-background py-2 px-4 gap-2 w-full duration-200'>
+        <nav className='sticky z-10 top-0 bg-lightMode-background flex flex-col justify-between border border-x-0 border-t-0 border-b-gray-400 dark:bg-darkMode-colors-background dark:border-b-gray-400 py-2 px-4 gap-2 w-full duration-200'>
             <div className='flex flex-row w-full justify-between gap-2'>
                 <div className='flex w-full gap-4 items-center'>
                     <Link to={`/`}>
@@ -47,7 +47,7 @@ function Nav({ handleSearch, toggleDarkmode, darkMode }: NavBarProps) {
                                 type="search"
                                 defaultValue={searchQuery} 
                                 className={`w-full border ${darkMode
-                                    ? 'dark:bg-darkMode-colors-background dark:text-darkMode-colors-text dark:border-darkMode-colors-text'
+                                    ? 'dark:bg-darkMode-colors-background dark:text-darkMode-colors-text dark:border-gray-400'
                                     : 'border-gray-400'} duration-200`}
                             />
                         </form>
