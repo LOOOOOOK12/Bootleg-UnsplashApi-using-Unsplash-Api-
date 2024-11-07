a98188a33c15955f7fc70b0e04689110eb2806a1
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-07 18:07:52 +0800

    ⚡️ refactored useSearch hook to remove unused state and integrate navigation on search

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,24 +1,44 @@
-import { useState, useEffect } from "react"; 
+import { useState, useRef } from "react"; 
 import * as PictureApi from '../api/pictureApi'
-
+import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export default function useSearch(){
-    const [ searchData, setSearchData ] = useState<any[]>([]); 
+    const [searchQuery, setSearchQuery] = useState<string>('');
+    // const [ searchData, setSearchData ] = useState<any[]>([])
+    const navigate = useNavigate();; 
+    // const [ searchQuery, setSearchQuery] = useState<string>("");
+    // const searchInputRef = useRef<HTMLInputElement>(null);
+    // const navigate = useNavigate();
 
     const handleSearch = async (value:string) => {
         if(value.trim() === ""){
-            setSearchData([]);
+            // setSearchData([]);
             return;
         } else {
+
             try {
-                const searchResult = await PictureApi.searchPictures(value);
-                setSearchData(searchResult);
-                console.log(searchResult)
+                navigate(`/search/${value}`);
+                // const searchResult = await PictureApi.searchPictures(value);
+                // setSearchData(searchResult);
+                // console.log(searchResult)
             } catch (error) {
                 console.log(error);
             }
         }
-    }   
+    }
 
-    return{ handleSearch, searchData}
+    // const onSearchSubmit = (event: React.FormEvent) => {
+    //     event.preventDefault();
+    //     if (handleSearch && searchInputRef.current) {
+    //         const query = searchInputRef.current.value;
+    //         if (query) {
+    //             setSearchQuery(query);  
+    //             handleSearch(query);   
+    //             navigate(`/search/${query}`);  
+    //         }
+    //     }
+    // }
+    
+    return{ handleSearch, searchQuery, setSearchQuery }
 }
