6557a6d82667530fb717be9306e0e46b97ab81b4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-08 16:09:26 +0800

    ⚡️ simplified main.tsx by removing unused components and props, and integrated App with React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,79 +1,11 @@
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.tsx";
-import PicturePage from "./pages/picturePage.tsx";
-import SearchPage from "./pages/searchPage.tsx";
-import CollectionsPage from "./pages/collectionsPage.tsx";
-import CollectionPage from "./pages/collectionPage.tsx";
-import Topicsgallery from "./pages/topicsgallery.tsx";
-import { useState } from "react";
-import NavBar from "./components/nav.tsx";
-import { ReactNode } from "react";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
-function PageLayout({
-  children,
-  toggleDarkmode,
-  darkMode,
-}: {
-  children: ReactNode;
-  toggleDarkmode: () => void;
-  darkMode: boolean;
-}) {
-  return (
-    <>
-      <NavBar toggleDarkmode={toggleDarkmode} darkMode={darkMode} />
-    </>
-  );
-}
-
-
-function Main() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkmode = () => {
-    setDarkMode((prevMode) => !prevMode);
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element:<App darkMode={darkMode} toggleDarkmode={toggleDarkmode} />,
-    },
-    {
-      path: "/photo/:pictureId",
-      element: (
-        <PicturePage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
-      ),
-    },
-    {
-      path: "/search/:query",
-      element: (
-        <SearchPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
-      ),
-    },
-    {
-      path: "/collections",
-      element: (
-        <CollectionsPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
-      ),
-    },
-    {
-      path: "/collections/:collectionsId/photos",
-      element: (
-        <CollectionPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
-      ),
-    },
-    {
-      path: "/topics/:slug/photos",
-      element: (
-        <Topicsgallery darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
-      ),
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-}
-
-createRoot(document.getElementById("root")!).render(<Main />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+);
