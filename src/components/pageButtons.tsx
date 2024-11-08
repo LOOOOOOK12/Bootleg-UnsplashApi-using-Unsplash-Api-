4315c8b0bcdaec2cedf0dd29f2f5e79e9ff713bd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-08 16:13:31 +0800

    ⚡️ updated button border thickness in pageButtons component for improved aesthetics

--- src/components/pageButtons.tsx
+++ src/components/pageButtons.tsx
@@ -4,8 +4,8 @@ import { ChevronLeft, ChevronRight  } from 'lucide-react';
 function pageButtons({ toggleNextPage, togglePrevPage, darkMode }:PageButtonProps) {
     return (
         <div className='fixed bottom-5 right-5 flex gap-2 z-20 '>
-            <button onClick={togglePrevPage} className={`border-2 border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Previous Page'>{ darkMode ?<ChevronLeft color='#f9fafb' size={50}/> : <ChevronLeft color='#040506' size={50}/>}</button>
-            <button onClick={toggleNextPage} className={`border-2 border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Next Page'>{ darkMode ?<ChevronRight color='#f9fafb' size={50}/> : <ChevronRight color='#040506' size={50}/>}</button>
+            <button onClick={togglePrevPage} className={`border-[1.5px] border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Previous Page'>{ darkMode ?<ChevronLeft color='#f9fafb' size={50}/> : <ChevronLeft color='#040506' size={50}/>}</button>
+            <button onClick={toggleNextPage} className={`border-[1.5px] border-[#040506] rounded-sm dark:border-[#f9fafb]`} title='Next Page'>{ darkMode ?<ChevronRight color='#f9fafb' size={50}/> : <ChevronRight color='#040506' size={50}/>}</button>
         </div>
     )
 }
