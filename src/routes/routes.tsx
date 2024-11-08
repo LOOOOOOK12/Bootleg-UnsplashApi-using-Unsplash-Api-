f88aa3625c5eab1f8620d85dfab4366d781ed6a7
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-08 16:10:14 +0800

    ⚡️ added routes configuration for main, picture, search, collections, collection, and topics pages with dark mode support

---
+++ src/routes/routes.tsx
@@ -0,0 +1,66 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "@/pages/mainpage.tsx";
+import PicturePage from "../pages/picturePage.tsx";
+import SearchPage from "../pages/searchPage.tsx";
+import CollectionsPage from "../pages/collectionsPage.tsx";
+import CollectionPage from "../pages/collectionPage.tsx";
+import Topicsgallery from "../pages/topicsgallery.tsx";
+import Layout from "../layout/Layout";
+import { NavBarProps } from "@/types/types.ts";
+
+function routes({ darkMode, toggleDarkmode }: NavBarProps) {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Layout darkMode={darkMode} toggleDarkmode={toggleDarkmode}>
+          <MainPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
+        </Layout>
+      ),
+    },
+    {
+      path: "/photo/:pictureId",
+      element: (
+        <Layout darkMode={darkMode} toggleDarkmode={toggleDarkmode}>
+          <PicturePage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
+        </Layout>
+      ),
+    },
+    {
+      path: "/search/:query",
+      element: (
+        <Layout darkMode={darkMode} toggleDarkmode={toggleDarkmode}>
+          <SearchPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
+        </Layout>
+      ),
+    },
+    {
+      path: "/collections",
+      element: (
+        <Layout darkMode={darkMode} toggleDarkmode={toggleDarkmode}>
+          <CollectionsPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
+        </Layout>
+      ),
+    },
+    {
+      path: "/collections/:collectionsId/photos",
+      element: (
+        <Layout darkMode={darkMode} toggleDarkmode={toggleDarkmode}>
+          <CollectionPage darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
+        </Layout>
+      ),
+    },
+    {
+      path: "/topics/:slug/photos",
+      element: (
+        <Layout darkMode={darkMode} toggleDarkmode={toggleDarkmode}>
+          <Topicsgallery darkMode={darkMode} toggleDarkmode={toggleDarkmode} />
+        </Layout>
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default routes;
