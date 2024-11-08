56b3b016c840665d3ef766651346df96e9f3256d
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-08 16:09:35 +0800

    ⚡️ cleaned up useSearch hook by removing commented-out code and unused variables for better clarity and maintainability

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,44 +1,17 @@
-import { useState, useRef } from "react"; 
-import * as PictureApi from '../api/pictureApi'
+import { useState } from "react"; 
 import { useNavigate } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
 
 export default function useSearch(){
     const [searchQuery, setSearchQuery] = useState<string>('');
-    // const [ searchData, setSearchData ] = useState<any[]>([])
-    const navigate = useNavigate();; 
-    // const [ searchQuery, setSearchQuery] = useState<string>("");
-    // const searchInputRef = useRef<HTMLInputElement>(null);
-    // const navigate = useNavigate();
+    const navigate = useNavigate(); 
 
     const handleSearch = async (value:string) => {
-        if(value.trim() === ""){
-            // setSearchData([]);
-            return;
-        } else {
-
             try {
                 navigate(`/search/${value}`);
-                // const searchResult = await PictureApi.searchPictures(value);
-                // setSearchData(searchResult);
-                // console.log(searchResult)
             } catch (error) {
                 console.log(error);
             }
-        }
-    }
-
-    // const onSearchSubmit = (event: React.FormEvent) => {
-    //     event.preventDefault();
-    //     if (handleSearch && searchInputRef.current) {
-    //         const query = searchInputRef.current.value;
-    //         if (query) {
-    //             setSearchQuery(query);  
-    //             handleSearch(query);   
-    //             navigate(`/search/${query}`);  
-    //         }
-    //     }
-    // }
-    
+        
+    }    
     return{ handleSearch, searchQuery, setSearchQuery }
 }
