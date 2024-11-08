703643063d2d180d5ca1308743384d41f19e231b
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-08 16:09:59 +0800

    ⚡️ refactored PicturePage component by removing unused NavBar and navigate functionality, adding conditional rendering for user profile and content availability

--- src/pages/picturePage.tsx
+++ src/pages/picturePage.tsx
@@ -1,11 +1,10 @@
 import { NavBarProps} from '@/types/types';
-import NavBar from '../components/nav';
-import { ThumbsUpIcon } from 'lucide-react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { CircleUserRound, ThumbsUpIcon } from 'lucide-react';
+import { useLocation } from 'react-router-dom';
 
-function PicturePage({ darkMode, toggleDarkmode }: NavBarProps) {
+
+function PicturePage({ darkMode}: NavBarProps) {
     const location = useLocation();
-    const navigate = useNavigate(); 
     const state = location.state;
     console.log(state);
 
@@ -15,32 +14,21 @@ function PicturePage({ darkMode, toggleDarkmode }: NavBarProps) {
 
     const { image, title , description, likes, userPfp, user, forHire} = state;
 
-    // const handleSearchAndNavigate = (newQuery: string) => {
-    //     if (handleSearch) {
-    //         handleSearch(newQuery);
-    //         navigate(`/search/${newQuery}`);
-    //     }
-    // };
-
     return (
         <div className={`relative bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
-            {/* <NavBar
-                toggleDarkmode={toggleDarkmode}
-                darkMode={darkMode}
-            /> */}
             <div className={`min-h-full text-lightMode-text bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text flex flex-col gap-3 px-5 py-8`}>
                 <div className='flex gap-2 items-center text-sm'>
-                    <img src={userPfp} alt={user} className='rounded-full'/>
+                    { userPfp ? <img src={userPfp} alt={user} className='rounded-full'/> :<CircleUserRound size={30}/>}
                     <div className='flex flex-col'>
-                        <h1 className='font-semibold'>{user}</h1>
-                        <p className={`${forHire ? `text-green-400`:`text-red-500`}`}>{forHire? `For hire` : `Not for hire`}</p>
+                        { user ? <h1 className='font-semibold'>{user}</h1> :<p> No user available</p>}
+                        <p className={`${forHire ? `text-green-400`:`text-red-500`}`}>{forHire? `For hire` : `Not Available`}</p>
                     </div>
                 </div>
                 <div className='flex flex-col justify-center items-center gap-4'>
                     <img src={image} alt={title} className='h-96 object-cover' />
                     <div className='flex flex-col items-start'>
-                        <p>{title}</p>
-                        <p>{description}</p>
+                        {title ? <p>{title}</p> : <p>No title available</p>}
+                        { description ? <p>{description}</p> : <p>No description available</p>}
                         <p className='flex gap-2'><ThumbsUpIcon/>{likes}</p>
                     </div>
                 </div>
