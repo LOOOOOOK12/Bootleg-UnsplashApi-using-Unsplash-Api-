5856aee2310b5dc1c507904cee469b3299a60b7c
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-09 21:04:24 +0800

    ⚡️ refactored JSX structure in mainpage component for improved readability

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -20,34 +20,34 @@ function mainpage({ darkMode }: DarkModeProps) {
             className="flex flex-wrap justify-center gap-3 px-4 py-8 dark:bg-darkMode-colors-background duration-200"
         >
             {photosData.map((pic) =>
-            isLoading ? (
-                <Blurhash hash={pic.blur_hash} punch={1} />
-            ) : (
-                <Link
-                key={pic.id}
-                to={`/photo/${pic.id}`}
-                state={{
-                    image: pic.urls.regular,
-                    title: pic.alt_description,
-                    description: pic.description || "No description",
-                    place: pic.location ? pic.location.name : "Unknown",
-                    likes: pic.likes || "0",
-                    userPfp: pic.user.profile_image.small,
-                    user: pic.user.username,
-                    forHire: pic.for_hire,
-                }}
-                >
-                <div className="relative">
-                    <img
-                    id={pic.id}
-                    src={pic.urls.raw + "&auto=format&w=1080&h=300&fit=fill"}
-                    alt={pic.description || "Image"}
-                    title={pic.alt_description || "No description"}
-                    className="object-cover"
-                    />
-                </div>
-                </Link>
-            )
+                isLoading ? (
+                    <Blurhash hash={pic.blur_hash} punch={1} />
+                ) : (
+                    <Link
+                    key={pic.id}
+                    to={`/photo/${pic.id}`}
+                    state={{
+                        image: pic.urls.regular,
+                        title: pic.alt_description,
+                        description: pic.description || "No description",
+                        place: pic.location ? pic.location.name : "Unknown",
+                        likes: pic.likes || "0",
+                        userPfp: pic.user.profile_image.small,
+                        user: pic.user.username,
+                        forHire: pic.for_hire,
+                    }}
+                    >
+                    <div className="relative">
+                        <img
+                        id={pic.id}
+                        src={pic.urls.raw + "&auto=format&w=1080&h=300&fit=fill"}
+                        alt={pic.description || "Image"}
+                        title={pic.alt_description || "No description"}
+                        className="object-cover"
+                        />
+                    </div>
+                    </Link>
+                )
             )}
         </div>
         <PageButtons
