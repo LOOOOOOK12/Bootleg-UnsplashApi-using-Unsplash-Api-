fe122b9eec3d273e9f6de455dd005bf41c04f5cd
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-11 18:49:37 +0800

    ⚡️ integrated Blurhash for loading state in SearchPage component

--- src/pages/searchPage.tsx
+++ src/pages/searchPage.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import * as PictureApi from '../api/pictureApi';
-import { Skeleton } from '../components/ui/skeleton';
 import { NavBarProps } from '@/types/types';
 import PageButtons from '@/components/pageButtons';
 import usePage from '@/hooks/usePage';
+import { Blurhash } from 'react-blurhash';
 
 function SearchPage({ darkMode }: NavBarProps) {
     const { query } = useParams<{ query: string }>();
@@ -32,32 +32,33 @@ function SearchPage({ darkMode }: NavBarProps) {
     return (
         <div className={`flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
             <div className="max-h-full flex flex-wrap overflow-hidden justify-center gap-5 py-8 px-5 dark:bg-darkMode-colors-background duration-200">
-                {isLoading ? (
-                    Array(30)
-                        .fill(0)
-                        .map((_, idx) => <Skeleton key={idx} className="h-52 w-60" />)
-                ) : (
-                    searchData.map((searchPic) => (
-                        <Link
-                            key={searchPic.id}
-                            to={`/photo/${searchPic.id}`}
-                            state={{
-                                image: searchPic.urls.regular,
-                                imageDescription: searchPic.alt_description,
-                                place: searchPic.location ? searchPic.location.name : 'Unknown',
-                                color: searchPic.color,
-                                likes: searchPic.likes || "0"
-                            }}
-                        >
-                            <img
-                                id={searchPic.id}
-                                src={searchPic.urls.regular}
-                                alt={searchPic.alt_description || 'Image'}
-                                title={searchPic.description || 'No description'}
-                                className="w-full h-80 grow"
-                            />
-                        </Link>
-                    ))
+                { searchData.map((searchPic) =>
+                    isLoading ? (
+                        <Blurhash
+                            hash={searchPic.blur_hash}
+                            width={300}
+                            height={250}
+                            punch={1}
+                        />
+                    ) : ( <Link
+                        key={searchPic.id}
+                        to={`/photo/${searchPic.id}`}
+                        state={{
+                            image: searchPic.urls.regular,
+                            imageDescription: searchPic.alt_description,
+                            place: searchPic.location ? searchPic.location.name : 'Unknown',
+                            color: searchPic.color,
+                            likes: searchPic.likes || "0"
+                        }}
+                    >
+                        <img
+                            id={searchPic.id}
+                            src={searchPic.urls.regular}
+                            alt={searchPic.alt_description || 'Image'}
+                            title={searchPic.description || 'No description'}
+                            className="w-full h-80 grow"
+                        />
+                    </Link>)
                 )}
             </div>
             <PageButtons toggleNextPage={handleNextPage} togglePrevPage={handlePrevPage} darkMode={darkMode}/>
