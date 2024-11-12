4533e470e384d47ea103ea977ab174acbcc77739
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-12 18:45:09 +0800

    ✨ integrated useGetPhoto hook for fetching and displaying photo data in PicturePage component

--- src/pages/picturePage.tsx
+++ src/pages/picturePage.tsx
@@ -1,38 +1,55 @@
-import { NavBarProps} from '@/types/types';
-import { CircleUserRound, ThumbsUpIcon } from 'lucide-react';
-import { useLocation } from 'react-router-dom';
-
+import { useParams } from "react-router-dom";
+import { NavBarProps } from "@/types/types";
+import { CircleUserRound, ThumbsUpIcon } from "lucide-react";
+import useGetPhoto from "@/hooks/useGetPhoto";
 
 function PicturePage({ darkMode }: NavBarProps) {
-    const location = useLocation();
-    const state = location.state;
-    console.log(state);
-
-    if (!state) {
-        return <div>Error: No picture data available</div>; 
-    }
-
-    const { image, title , description, likes, userPfp, user, forHire, camera} = state;
+    const { pictureId } = useParams<{ pictureId: string }>();
+    const { photoData } = useGetPhoto(pictureId);
+    console.log(photoData);
 
     return (
-        <div className={`relative bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
-            <div className={`min-h-full text-lightMode-text bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text flex flex-col gap-3 px-5 py-8`}>
-                <div className='flex gap-2 items-center text-sm'>
-                    { userPfp ? <img src={userPfp} alt={user} className='rounded-full'/> :<CircleUserRound size={30}/>}
-                    <div className='flex flex-col'>
-                        { user ? <h1 className='font-semibold'>{user}</h1> :<p> No user available</p>}
-                        <p className={`${forHire ? `text-green-400`:`text-red-500`}`}>{forHire? `For hire` : `Not Available`}</p>
-                    </div>
-                </div>
-                <div className='flex flex-col justify-center items-center gap-4'>
-                    <img src={image} alt={title} className='h-96 object-cover' />
-                    <div className='flex flex-col items-start'>
-                        {title ? <p>{title}</p> : <p>No title available</p>}
-                        { description ? <p>{description}</p> : <p>No description available</p>}
-                        <p className='flex gap-2'><ThumbsUpIcon/>{likes}</p>
-                        <p>{camera}</p>
-                    </div>
-                </div>
+        <div className={`relative bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? "dark" : ""}`}>
+            <div className={`min-h-screen text-lightMode-text bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text flex flex-col items-center gap-4 px-5 py-8`}>
+                {!photoData ? (
+                    <div>Loading...</div>
+                ) : (
+                    <>
+                        <div className="w-full">
+                            <div className="flex gap-2 items-center text-sm">
+                                {photoData.user.profile_image ? (
+                                    <img src={photoData.user.profile_image.small} alt={photoData.username} className="rounded-full"/>
+                                ) : (
+                                    <CircleUserRound />
+                                )}
+                                <div className="flex flex-col">
+                                    {photoData.user.name ? (<h1 className="font-semibold">{photoData.user.name}</h1> ) : ( <h1 className="font-semibold">No user available</h1>)}
+                                    <p className={`${photoData.user.for_hire ? `text-green-400`: `text-red-400`}`}>
+                                        {photoData.user.for_hire ? "For Hire" : "Not Available"}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <img src={photoData.urls.regular} alt={photoData.alt_description} className="h-96 w-60 object-cover"/>
+                        <div className="w-full flex flex-col items-start gap-4">
+                            <div className="flex flex-col gap-2">
+                                <p>{photoData.description ? photoData.description : "No Description Available"}</p>
+                                <p className="flex gap-2">
+                                    <ThumbsUpIcon/>
+                                    {photoData.likes}
+                                </p>
+                            </div>
+                            <div className="flex flex-col gap-2 w-full">
+                                <h1>Tags:</h1>
+                                <span className=" flex flex-wrap gap-2">
+                                    {photoData.tags.map((tag:any) => (
+                                        <a key={tag.title} className="border border-darkMode-colors-primary rounded-sm px-4 py-2 hover:bg-gray-300 dark:hover:bg-gray-700 duration-200">{tag.title}</a>
+                                    ))}
+                                </span>
+                            </div>                          
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
