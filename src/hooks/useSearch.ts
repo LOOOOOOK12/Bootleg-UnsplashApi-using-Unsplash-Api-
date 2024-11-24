ad0b3b845d3b84025a0ad954af3b6fe653e632f4
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-24 15:37:49 +0800

    ✨ added fetchSearchResults function to useSearch hook for improved search functionality and state management

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,8 +1,10 @@
 import { useState } from "react"; 
 import { useNavigate } from "react-router-dom";
+import * as PictureApi from '../api/pictureApi';
 
 export default function useSearch(){
     const [searchQuery, setSearchQuery] = useState<string>('');
+    const [ searchResultsData, setSearchResultsData ] = useState<any[]>([]);
     const navigate = useNavigate(); 
 
     const handleSearch = async (value:string) => {
@@ -11,7 +13,15 @@ export default function useSearch(){
             } catch (error) {
                 console.log(error);
             }
-        
-    }    
-    return{ handleSearch, searchQuery, setSearchQuery }
+    }
+    
+    const fetchSearchResults = async (query:string, page: Number) => {
+        try {
+            const result = await PictureApi.searchPictures(query,page);
+            setSearchResultsData(result);
+        } catch (error) {
+            console.log("Error fetching search results:", error);
+        }
+    }
+    return{ handleSearch, searchQuery, setSearchQuery, fetchSearchResults, searchResultsData };
 }
