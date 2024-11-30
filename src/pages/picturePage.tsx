7b2d0552fad20ba75118f1669788c2c1464fd8fa
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-11-30 23:27:20 +0800

    ✨ enhanced PicturePage with related images section and improved metadata display for better user engagement

--- src/pages/picturePage.tsx
+++ src/pages/picturePage.tsx
@@ -1,15 +1,22 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { NavBarProps } from "@/types/types";
-import { CircleUserRound, ThumbsUpIcon, Camera, Download } from "lucide-react";
+import { CircleUserRound, ThumbsUpIcon, Camera, Download, Eye, CalendarClock, Captions, Focus } from "lucide-react";
 import useGetPhoto from "@/hooks/useGetPhoto";
 import useSearch from "@/hooks/useSearch";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect } from "react";
 
 function PicturePage({ darkMode }: NavBarProps) {
     const { pictureId } = useParams<{ pictureId: string }>();
-    const { photoData } = useGetPhoto(pictureId);
-    const { handleSearch } = useSearch();
+    const { photoData } = useGetPhoto( pictureId );
+    const { handleSearch, fetchSearchResults, searchResultsData } = useSearch();
     console.log(photoData);
+
+    useEffect(() => {
+        if (photoData && photoData.tags && photoData.tags.length > 0) {
+            fetchSearchResults(photoData.tags[0].title);
+        }
+    }, [photoData]);
 
     return (
         <div className={`relative bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? "dark" : ""}`}>
@@ -52,21 +59,31 @@ function PicturePage({ darkMode }: NavBarProps) {
                         <div className="w-full flex flex-col items-start gap-4">
                             <div className="w-full flex flex-col gap-3 md:flex-row">
                                 <div className="w-full flex flex-col gap-2 border border-gray-400 rounded-sm p-2">
-                                    <h1>{photoData.description ? photoData.description : photoData.alt_description}</h1>
+                                    <span className="flex gap-2">
+                                        <span title="Description"><Captions/></span>
+                                        <h1>{photoData.description ? photoData.description : photoData.alt_description}</h1>
+                                    </span>
                                     <span className="flex gap-2 items-center">
                                         <span title="Total of Likes"><ThumbsUpIcon size={20}/></span>
                                         {photoData.likes}
                                     </span>
-                                    <h1>Published in:   
-                                        {new Date(photoData.updated_at).toLocaleDateString("en-US", {
-                                        year: "numeric",
-                                        month: "long",
-                                        day: "numeric"
-                                        })}
-                                    </h1>
+                                    <span className="flex gap-2">
+                                        <span title="Date Uploaded"><CalendarClock/></span>
+                                        <h1> 
+                                            {new Date(photoData.updated_at).toLocaleDateString("en-US", {
+                                            year: "numeric",
+                                            month: "long",
+                                            day: "numeric"
+                                            })}
+                                        </h1>
+                                    </span>
                                     <span className="flex gap-2 items-center">
                                         <span title="Total Downloads"><Download/></span>
                                         {photoData.downloads}
+                                    </span>
+                                    <span className="flex gap-2 items-center">
+                                        <span title="Total views"><Eye/></span>
+                                        {photoData.views}
                                     </span>
                                 </div>
                                 <div className="w-full flex flex-col gap-2 border border-gray-400 rounded-sm p-2">
@@ -79,8 +96,9 @@ function PicturePage({ darkMode }: NavBarProps) {
                                         )}
                                     </span>
                                     <span className="flex gap-2"> 
+                                        <span title="Lens"><Focus/></span>
                                         <span className="flex-col">
-                                            <h1>Lens: {photoData.exif.aperture ?? "N/A"}, {photoData.exif.exposure_time ?? "N/A"}, {photoData.exif.focal_length ?? "N/A"}, {photoData.exif.iso ?? "N/A"}</h1>
+                                            <h1>{photoData.exif.aperture ?? "N/A"}, {photoData.exif.exposure_time ?? "N/A"}, {photoData.exif.focal_length ?? "N/A"}, {photoData.exif.iso ?? "N/A"}</h1>
                                         </span>
                                     </span>
                                 </div>
@@ -98,10 +116,23 @@ function PicturePage({ darkMode }: NavBarProps) {
                                     ))}
                                 </div>
                             </div>
-                            <div>
+                            <div className="flex flex-col w-full gap-2">
                                 <h1>Related Images</h1>
-                                <div className="flex flex-wrap justify-center gap-3 px-4 py-8 dark:bg-darkMode-colors-background duration-200">
-                                    
+                                <div className="flex flex-wrap justify-center gap-3 dark:bg-darkMode-colors-background duration-200">
+                                    {searchResultsData.map((relPic) =>(
+                                        <Link
+                                            to={`/photos/${relPic.id}`}
+                                            key={relPic.id}
+                                        >
+                                            <img
+                                            key={relPic.id}
+                                            src={relPic.urls.regular}
+                                            alt={relPic.alt_description}
+                                            className=" h-80 object-cover"
+                                            /> 
+                                        </Link>
+                                        
+                                    ))}
                                 </div>
                             </div>                          
                         </div>
