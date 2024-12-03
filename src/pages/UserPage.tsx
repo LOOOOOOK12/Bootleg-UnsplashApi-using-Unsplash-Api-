efb737deb2a4197744c4ba3cf14669acf23d7193
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-12-03 21:12:26 +0800

    ✨ enhanced UserPage layout with improved image display and user information presentation

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -12,16 +12,23 @@ function UserPage({ darkMode }: NavBarProps) {
 
     return (
         <div className={` ${darkMode ? "dark" : ""}`}>
-            <div className="h-screen px-4 py-2 text-lightMode-text flex bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text">
+            <div className="h-screen text-lightMode-text flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text">
                 {isLoading ? (
                     <Skeleton className="h-40 w-60" />
                 ) : (
                     userInfo && (
-                        <div>
-                            <img src={userInfo.profile_image.medium} alt={userInfo.username} className="h-11 w-20" />
-                            <div className="flex flex-col">
-                                <h1>{userInfo.name}</h1>
-                                <p>{userInfo.bio}</p>
+                        <div className="w-full">
+                            <div className="relative w-full flex items-center justify-center h-80">
+                                <div className="h-full w-full flex flex-row relative z-10 overflow-hidden">
+                                    <img src={userInfo.photos[0].urls.regular} alt="" className="w-full h-full object-cover brightness-50 hover:brightness-75 duration-100"/>
+                                    <img src={userInfo.photos[1].urls.regular} alt="" className="w-full h-full object-cover brightness-50 hover:brightness-75 duration-100"/>
+                                    <img src={userInfo.photos[2].urls.regular} alt="" className="w-full h-full object-cover brightness-50 hover:brightness-75 duration-100"/>
+                                </div>
+                                <h1 className="absolute font-bold text-4xl z-20 md:text-6xl lg:text-8xl text-darkMode-colors-text">{userInfo.name}</h1>
+                                <img src={userInfo.profile_image.large} alt={userInfo.username} className="size-40 rounded-full z-30 absolute -bottom-16 md:left-5 lg:left-5" />  
+                            </div>
+                            <div className="flex flex-col gap-2 p-4 mt-32">
+                                <p>{userInfo.bio || "No bio"}</p>
                             </div>
                         </div>
                     )
