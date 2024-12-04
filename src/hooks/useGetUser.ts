01ee6e6b4322cc172bd2265b237e0ac33686309a
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-12-04 18:13:12 +0800

    ✨ added hooks to fetch user photos, collections, and liked photos in useGetUser

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -3,6 +3,9 @@ import * as PictureApi from '../api/pictureApi';
 
 export default function useGetUser( username:any ){
     const [ userInfo, setUserInfo ] = useState<any>(null);
+    const [ userPhotos, setUserPhotos] = useState<any[]>([]);
+    const [ userCollections, setUserCollections ] = useState<any[]>([]);
+    const [ userLikedPhotos, setUserLikedPhotos ] = useState<any[]>([]);
     const [ isLoading, setIsLoading ] = useState<boolean>(false);
 
     const fetchUser = async () => {
@@ -17,9 +20,39 @@ export default function useGetUser( username:any ){
         }
     }
 
+    const fetchUserPhotos = async (username:string) => {
+        try {
+            const result = await PictureApi.getUserPhotos(username);
+            setUserPhotos(result);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const fetchUserColections = async (username: string) => {
+        try {
+            const result = await PictureApi.getUserCollections(username);
+            setUserCollections(result);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchUserLikedPhotos = async (username: string) => {
+        try {
+            const result = await PictureApi.getUserLikedPhotos(username);
+            setUserLikedPhotos(result);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     useEffect(()=>{
         fetchUser();
+        fetchUserPhotos(username);
+        fetchUserColections(username);
+        fetchUserLikedPhotos(username);
     },[username]);
 
-    return { userInfo, isLoading };
+    return { userInfo, isLoading, userPhotos, userCollections, userLikedPhotos };
 }
