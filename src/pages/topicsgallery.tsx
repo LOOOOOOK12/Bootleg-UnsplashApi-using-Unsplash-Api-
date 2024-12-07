a085f47528010c02336ccc03f098b90365ab5930
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-12-07 18:51:00 +0800

    fix: update TopicsGallery layout to improve column responsiveness on different screen sizes

--- src/pages/topicsgallery.tsx
+++ src/pages/topicsgallery.tsx
@@ -13,7 +13,7 @@ function TopicsGallery({ darkMode }: DarkModeProps) {
 
     return (
         <div className={`relative flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
-            <div id="Home" className="content-evenly columns-3 justify-center gap-2 px-4 py-8 dark:bg-darkMode-colors-background duration-200" >
+            <div id="Home" className="content-evenly columns-1 md:columns-3 lg:columns-4 justify-center gap-2 px-4 py-8 dark:bg-darkMode-colors-background duration-200" >
                 {topicPhotos.map((pic) => 
                 isLoading ? (
                     <Skeleton className="w-64 h-80" />
