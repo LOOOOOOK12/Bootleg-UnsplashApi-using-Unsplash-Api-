6d2d8cd37068e382c8809d63eec35b7daa6f0549
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-12-09 14:56:13 +0800

    fix: update collectionPage to use username instead of user and improve layout responsiveness

--- src/pages/collectionPage.tsx
+++ src/pages/collectionPage.tsx
@@ -10,12 +10,8 @@ function collectionPage({ darkMode }: NavBarProps) {
     const location = useLocation();
     const { page, handleNextPage, handlePrevPage } = usePage(1);
     const state = location.state;
-    const { id, image, title, description, totalPhotos, user, pfp } = state;
+    const { id, image, title, description, totalPhotos, username, pfp } = state;
     const { isloading, collectionPhotos } = useGetCollectionPhoto(page, id);
-
-    console.log(state);
-    console.log(collectionPhotos);
-
     
     if(!state){
         return <div className='h-screen items-center justify-center'>Error: No collection data available</div>; 
@@ -23,19 +19,19 @@ function collectionPage({ darkMode }: NavBarProps) {
 
     return (
         <div className={`relative bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
-            <div className=' flex flex-col items-center gap-3 px-4 py-8 bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text'>
+            <div className={`${collectionPhotos ? "w-full flex flex-col items-center gap-3 px-4 py-8 bg-lightMode-background dark:bg-darkMode-colors-background dark:text-darkMode-colors-text" : "h-screen"}`}>
                     <img src={image} alt={title} title={title} className='h-40 w-56 rounded-full object-cover'/>
                 <div className='flex flex-col items-center gap-3'>
                     <h1 className='font-bold text-5xl'>{title}</h1>
                     <Link 
-                        to={`/users/${user}`}>
-                        <h2 className='flex gap-2 items-center text-xl'>{pfp? <img src={pfp} alt={user} className='rounded-full'/> : <CircleUserRound/> } {user}</h2>
+                        to={`/users/${username}`}>
+                        <h2 className='flex gap-2 items-center text-xl'>{pfp? <img src={pfp} alt={username} className='rounded-full'/> : <CircleUserRound/> } {username}</h2>
                     </Link>
                     <h3 className='text-xl'>{description}</h3>
                 </div>
                 <div className='w-full h-full flex flex-col gap-3'>
                     <h1 className='items-start font-normal'>Photos: {totalPhotos}</h1>
-                    <div className='flex flex-wrap justify-center gap-2 overflow-hidden'>
+                    <div className='gap-2 p-3 content-evenly columns-1 md:columns-3 lg:columns-4'>
                         {collectionPhotos.map((pic) => 
                             isloading ? (
                                 <Skeleton className='w-64 h-80'/>
@@ -46,8 +42,8 @@ function collectionPage({ darkMode }: NavBarProps) {
                                         image:pic.urls.regular,
                                     }}
                                     >
-                                    <img src={pic.urls.regular + "&auto=format&w=1080&h=300&fit=fill&auto=format"} alt=""
-                                        className='object-cover'
+                                    <img src={pic.urls.regular + "&auto=format"} alt=""
+                                        className='object-cover mb-2'
                                     />
                                 </Link>
                             )
