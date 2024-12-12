380508f0dd8d85690fe3fac669a6f183190b4fcb
Luke Symon A. Alamil <alamil.266740@legazpi.sti.edu.ph>
2024-12-12 19:16:17 +0800

    fix: enhance search results display with loading skeleton and no results message

--- src/pages/searchPage.tsx
+++ src/pages/searchPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import { Frown } from 'lucide-react';
 import { NavBarProps } from '@/types/types';
 import PageButtons from '@/components/pageButtons';
 import usePage from '@/hooks/usePage';
@@ -9,46 +10,56 @@ import useSearch from '@/hooks/useSearch';
 function SearchPage({ darkMode }: NavBarProps) {
     const { query } = useParams<{ query: string }>();
     const [isLoading, setIsLoading] = useState<boolean>(true);
-    const { page, handleNextPage, handlePrevPage} = usePage(1);
+    const { page, handleNextPage, handlePrevPage } = usePage(1);
     const { fetchSearchResults, searchResultsData } = useSearch();
 
     useEffect(() => {
+        const fetchResults = async () => {
             if (query) {
                 try {
                     setIsLoading(true);
-                    fetchSearchResults(query, page);
+                    await fetchSearchResults(query, page);
                 } catch (error) {
                     console.log("Error fetching search results:", error);
                 } finally {
                     setIsLoading(false);
                 }
+            }
         };
+        fetchResults();
     }, [query, page]);
 
     return (
         <div className={`flex flex-col bg-lightMode-background dark:bg-darkMode-colors-background ${darkMode ? 'dark' : ''}`}>
             <div className="max-h-full flex flex-wrap overflow-hidden justify-center gap-5 py-8 px-5 dark:bg-darkMode-colors-background duration-200">
-                {searchResultsData.map((searchPic) =>
-                    isLoading ? (
-                        <Skeleton className='h-40 w-60'/>
-                    ) : ( <Link
-                        key={searchPic.id}
-                        to={`/photos/${searchPic.id}`}
-                        state={{
-                            id: searchPic.id,
-                        }}
-                    >
-                        <img
-                            id={searchPic.id}
-                            src={searchPic.urls.regular}
-                            alt={searchPic.alt_description || 'Image'}
-                            title={searchPic.description || 'No description'}
-                            className="w-full h-80 object-cover"
-                        />
-                    </Link>)
+                {isLoading ? (
+                    Array(30).fill(0).map((_, idx) => <Skeleton key={idx} className="h-40 w-60" />)
+                ) : searchResultsData.length > 0 ? (
+                    searchResultsData.map((searchPic) => (
+                        <Link
+                            key={searchPic.id}
+                            to={`/photos/${searchPic.id}`}
+                            state={{
+                                image: searchPic.urls.regular,
+                                title: searchPic.alt_description,
+                                description: searchPic.description || 'No description',
+                                place: searchPic.location ? searchPic.location.name : 'Unknown',
+                                likes: searchPic.likes || '0',
+                            }}
+                        >
+                            <img
+                                src={searchPic.urls.regular}
+                                alt={searchPic.alt_description || 'Image'}
+                                title={searchPic.description || 'No description'}
+                                className="w-full h-80 grow"
+                            />
+                        </Link>
+                    ))
+                ) : (
+                    <p className='h-screen w-full flex flex-col justify-center items-center gap-4'> <Frown size={50}/> No results found...</p>
                 )}
             </div>
-            <PageButtons toggleNextPage={handleNextPage} togglePrevPage={handlePrevPage} darkMode={darkMode}/>
+            <PageButtons toggleNextPage={handleNextPage} togglePrevPage={handlePrevPage} darkMode={darkMode} />
         </div>
     );
 }
